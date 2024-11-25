f4dbb20f9f3dc31e0740a2c2c49d32984a1891e4
Harshit Chauhan <m4070k@gmail.com>
2024-11-25 15:34:44 +0530

    cgi support

--- server.py
+++ server.py
@@ -1,9 +1,34 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
+import subprocess
 
 class ServerException(Exception): pass
 
-class case_no_file(object):
+class base_class(object):
+    '''Parent for case handlers.'''
+
+    def handle_file(self, handler, full_path):
+        """Handles the serving of the requested file."""
+        try:
+            # bad idea
+            # reading the whole file into memory when serving
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            handler.send_content(content)
+        except IOError as msg:
+            msg = f"{full_path} cannot be read: {msg}"
+            handler.handle_error(msg)
+    
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    def test(self, handler):
+        assert False, 'Not implemented.'
+
+    def act(self, handler):
+        assert False, 'Not implemented.'
+
+class case_no_file(base_class):
     '''File or directory does not exist.'''
 
     def test(self, handler):
@@ -12,16 +37,16 @@ class case_no_file(object):
     def act(self, handler):
         raise ServerException(f"{handler.path} not found")
 
-class case_existing_file(object):
+class case_existing_file(base_class):
     '''File exists.'''
 
     def test(self, handler):
         return os.path.isfile(handler.full_path)
     
     def act(self, handler):
-        handler.handle_file(handler.full_path)
+        self.handle_file(handler, handler.full_path)
 
-class case_always_fail(object):
+class case_always_fail(base_class):
     '''Base case.'''
 
     def test(self, handler):
@@ -30,39 +55,71 @@ class case_always_fail(object):
     def act(self, handler):
         raise ServerException(f"Unknown object {handler.path}")
 
-class case_directory_index_file(object):
+class case_directory_index_file(base_class):
     '''Serve index.html page for a directory.'''
-
-    def index_path(self, handler):
-        return os.path.join(handler.full_path, 'index.html')
 
     def test(self, handler):
         return os.path.isdir(handler.full_path) and \
                os.path.isfile(self.index_path(handler))
     
     def act(self, handler):
-        handler.handle_file(self.index_path(handler))
+        self.handle_file(handler, self.index_path(handler))
 
-class case_directory_no_index_file(object):
+class case_directory_no_index_file(base_class):
     '''Serve listing for a directory without an index.html page.'''
 
-    def index_path(self, handler):
-        return os.path.join(handler.full_path, 'index.html')
+    Listing_Page = '''\
+    <html>
+    <body>
+    <h2>Accessing {0}</h2>
+    <ul>{1}</ul>
+    </body>
+    </html>
+    '''
+
+    def list_dir(self, handler):
+        try:
+            entries = os.listdir(handler.full_path)
+            bullets = [f"<li>{e}</li>" for e in entries if not e.startswith('.')]
+            page = self.Listing_Page.format(handler.path, '\n'.join(bullets))
+            handler.send_content(page.encode())
+        except OSError as msg:
+            msg = f"{handler.path} cannot be listed. {msg}"
+            handler.handle_error(msg)
 
     def test(self, handler):
         return os.path.isdir(handler.full_path) and \
                not os.path.isfile(self.index_path(handler))
     
     def act(self, handler):
-        handler.list_dir(handler.full_path)
+        self.list_dir(handler)
+
+class case_cgi_file(base_class):
+    '''Something runnable.'''
+    
+    def run_cgi(self, handler):
+        cmd = ["python", handler.full_path]
+
+        with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+            data, errors = proc.communicate()
+        
+        if errors:
+            handler.handle_error(errors)
+            return
+
+        handler.send_content(data)
+
+    def test(self, handler):
+        # if filepath endswith .py? if so, run this
+        return os.path.isfile(handler.full_path) and \
+               handler.full_path.endswith('.py')
+    
+    def act(self, handler):
+        self.run_cgi(handler)
 
 class RequestHandler(BaseHTTPRequestHandler):
-    '''
-    If the requested path maps to a file, that file is served.
-    If anything goes wrong, an error page is constructed.
-    '''
-
     Cases = [case_no_file, 
+             case_cgi_file,
              case_existing_file,
              case_directory_index_file,
              case_directory_no_index_file,
@@ -77,37 +134,6 @@ class RequestHandler(BaseHTTPRequestHandler):
     </body>
     </html>
     '''
-
-    Listing_Page = '''\
-    <html>
-    <body>
-    <h2>Accessing {0}</h2>
-    <ul>{1}</ul>
-    </body>
-    </html>
-    '''
-
-    def list_dir(self, full_path):
-        try:
-            entries = os.listdir(full_path)
-            bullets = [f"<li>{e}</li>" for e in entries if not e.startswith('.')]
-            page = self.Listing_Page.format(self.path, '\n'.join(bullets))
-            self.send_content(page.encode())
-        except OSError as msg:
-            msg = f"{self.path} cannot be listed. {msg}"
-            self.handle_error(msg)
-
-    def handle_file(self, full_path):
-        """Handles the serving of the requested file."""
-        try:
-            # bad idea
-            # reading the whole file into memory when serving
-            with open(full_path, 'rb') as reader:
-                content = reader.read()
-            self.send_content(content)
-        except IOError as msg:
-            msg = f"{self.path} cannot be read: {msg}"
-            self.handle_error(msg)
 
     def handle_error(self, msg):
         """Handles errors by sending a generic error page."""
