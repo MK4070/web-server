7ba50f05b01afee6245fd77d6fbb74edea0b8f79
Harshit Chauhan <m4070k@gmail.com>
2024-12-29 02:39:25 +0530

    added sample data

--- scripts/server.py
+++ scripts/server.py
@@ -1,9 +1,64 @@
-from http.server import HTTPServer
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import threading
 import os
-from request_handler import RequestHandler
 from utils.config_reader import Config
-from utils.logger import common_logger
+from utils.logger import common_logger, get_thread_logger
+from handler_cases import Cases
+
+
+class RequestHandler(BaseHTTPRequestHandler):
+    Error_page = """\
+    <html>
+    <body>
+    <h1>Error accessing {path}</h1>
+    <p>{msg}</p>
+    </body>
+    </html>
+    """
+
+    def handle_error(self, msg):
+        """Handles errors by sending a generic error page."""
+        content = self.Error_page.format(path=self.path, msg=msg)
+        self.send_content(content.encode(), 404)
+
+    def send_content(self, content, status=200):
+        self.send_response(status)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content)
+
+    # Handle a GET request.
+    def do_GET(self):
+        try:
+            self.full_path = os.getcwd() + self.path
+
+            for case in Cases:
+                handler = case()
+                if handler.test(self):
+                    handler.act(self)
+                    break
+
+        except Exception as msg:
+            self.handle_error(msg)
+
+    def log_request(self, code="-", size="-"):
+        log_message = (
+            f"Request from "
+            f"{self.client_address[0]}:{self.client_address[1]}"
+            f" - [{self.command} {self.path} {self.request_version}]"
+            f" - {code} {size}"
+        )
+        logger = get_thread_logger(self.server.name)
+        logger.info(log_message)
+
+        common_message = (
+            f"({self.server.name} - {self.server.num_requests})"
+            f" - {self.client_address[0]}:{self.client_address[1]}"
+            f" - [{self.command} {self.path} {self.request_version}]"
+            f" - {code} {size}"
+        )
+        common_logger.info(common_message)
 
 
 class Server(HTTPServer):
--- scripts/utils/logger.py
+++ scripts/utils/logger.py
@@ -1,9 +1,15 @@
 import logging
+import os
+from utils.config_reader import Config
+
+config = Config.get_config()
+log_directory = config.get("Server", "logDirectory")
+os.makedirs(os.path.dirname(log_directory), exist_ok=True)
 
 
 def setup_loggers():
     common_logger = logging.getLogger("common")
-    common_handler = logging.FileHandler("Logs/common.log", "w")
+    common_handler = logging.FileHandler(f"{log_directory}/common.log", "w")
     common_handler.setFormatter(
         logging.Formatter("[%(asctime)s] - %(levelname)s - %(message)s")
     )
@@ -12,7 +18,9 @@ def setup_loggers():
 
     def get_thread_logger(server_name):
         logger = logging.getLogger(server_name)
-        handler = logging.FileHandler(f"Logs/{server_name}.log", "w")
+        handler = logging.FileHandler(
+            f"../{log_directory}/{server_name}.log", "w"
+        )
         handler.setFormatter(
             logging.Formatter("[%(asctime)s] - %(levelname)s - %(message)s")
         )
